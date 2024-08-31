164a6f33d2b0c91ae639e9d52430b16fb34f8421
Thomas antony <thomasantony12042001@gmail.com>
2024-08-31 13:37:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.querySelector(".no").addEventListener("mouseover", function () {
-  var above = Math.floor(Math.random() * 750);
-  var lef = Math.floor(Math.random() * 750);
+  var above = Math.floor(Math.random() * 20);
+  var lef = Math.floor(Math.random() * 20);
   document.querySelector(".box").style.position = "absolute";
-  document.querySelector(".box").style.top = above + "px";
-  document.querySelector(".box").style.left = lef + "px";
+  document.querySelector(".box").style.top = above + "rem";
+  document.querySelector(".box").style.left = lef + "rem";
 });
 
 document.querySelector(".no").addEventListener("click", function () {
-  var above = Math.floor(Math.random() * 750);
-  var lef = Math.floor(Math.random() * 750);
+  var above = Math.floor(Math.random() * 20);
+  var lef = Math.floor(Math.random() * 10);
   document.querySelector(".box").style.position = "absolute";
-  document.querySelector(".box").style.top = above + "px";
-  document.querySelector(".box").style.left = lef + "px";
+  document.querySelector(".box").style.top = above + "rem";
+  document.querySelector(".box").style.left = lef + "rem";
 });
 
 document.querySelector(".yes").addEventListener("click", function () {
